abff2e6b92f095eb6fef4c83324f8223e2fc6dd3
Ant Weiss <anton@otomato.io>
2024-12-18 17:55:58 +0200

    Reformat

--- schemas.py
+++ schemas.py
@@ -2,19 +2,23 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import datetime
 
+
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
     full_name: Optional[str] = None
 
+
 class UserLogin(BaseModel):
     username: str
     password: str
 
+
 class UserUpdate(BaseModel):
     full_name: Optional[str] = None
     email: Optional[str] = None
+
 
 class UserResponse(BaseModel):
     id: int
